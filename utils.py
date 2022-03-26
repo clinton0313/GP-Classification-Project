8604e29e1ee60420384f9535a8eda43dc6d6c365
clinton0313 <clinton0313@gmail.com>
2022-03-26 16:34:34 +0100

    merged utils files.

--- utils.py
+++ utils.py
@@ -1,9 +1,51 @@
-# %%
 import matplotlib.pyplot as plt
 import numpy as np
 from typing import Callable
 from EllipticalSliceSampler import EllipticalSampler
-# %%
+
+def plot_gp_samples(
+    X, 
+    samples, 
+    ax = None, 
+    x_dim=0, 
+    color="tab:purple", 
+    alpha=0.3, 
+    plot_mean = True, 
+    mean_color="tab:red", 
+    **plot_kwargs
+) -> None:
+    '''
+    Plots sample.
+    Args:
+        X: Design matrix (nxd)
+        samples: Samples returned from GPC (num_samples x n)
+        ax: matplotlib ax to plot onto. If no axes is passed than a new figure will be returned.
+        color: Color of sample lines.
+        alpha: Alpha for sample line plots
+        plot_mean: Plots the mean posterior sample if true.
+        mean_color: Color of the mean line.
+    '''
+
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(10,10), tight_layout=True)
+
+    if len(X.shape) != 1:
+        X = X[:, x_dim].tolist()
+    samples = samples.squeeze()
+    for sample in samples:
+        x, y = zip(*sorted(zip(X, sample.tolist()), key=lambda i: i[0]))
+        ax.plot(x, y, color=color, alpha=alpha, **plot_kwargs)
+    
+    if plot_mean:
+        sample_mean = np.mean(samples, axis=0)
+        x, y = zip(*sorted(zip(X, sample_mean.tolist()), key=lambda i: i[0]))
+        ax.plot(x, y, color=mean_color)
+
+    try:
+        return fig
+    except:
+        pass
+
 def plot_sampler(sample:np.ndarray, ll:Callable, stepsize:int=25):
     """
     Plots the likelihood over posterior samples of the ESS.
@@ -21,7 +63,6 @@ def plot_sampler(sample:np.ndarray, ll:Callable, stepsize:int=25):
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
 
-# %%
 def plot_multiple(
     ess:Callable, 
     ll:Callable, 
